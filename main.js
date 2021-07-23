d174cb6d09df5f7eca580045dcb63eb482811b49
naaru2011 <83050539+naaru2011@users.noreply.github.com>
2021-07-23 16:43:25 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var first_name = "Narayan";
+console.log(first_name);
+var last_name = "Mitra";
+console.log(last_name);
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = "10";
+console.log(a);
+var b = "5";
+console.log(b);
+var add = a + b;
+console.log(add);
+var subract = a - b;
+console.log(subract);
+var multiply = a * b;
+console.log(multiply);
+var divide = a / b;
+console.log(divide);
